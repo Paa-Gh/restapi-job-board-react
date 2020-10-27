ac31b634aaeec4651eb4003c19a29e8e2bb073ab
Paa-Gh <67100385+Paa-Gh@users.noreply.github.com>
2020-10-27 21:58:59 +0000

    Update JobsList.js

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -90,7 +90,7 @@ const JobsList = props => {
         </div>
         <br/>
         <hr/>
-        {jobs.map((job, index) => (
+        {[jobs].map((job, index) => (
             <Job key={index} job={job} removeJob={removeJob} editJob={editJob} editing={editing} />
           ))}
       </div>
